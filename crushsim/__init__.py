eb1f06e8c2aee101b992583997956a0097b167e7
Xavier Villaneau <xvillaneau@gmail.com>
2016-04-26 18:55:52 -0400

    Very basic module structure

---
+++ crushsim/__init__.py
@@ -0,0 +1,8 @@
+
+from __future__ import print_function
+
+
+class CRUSHsim():
+
+    def __init__():
+        pass
---
+++ setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(name='crushsim', version='0.1', packages=['crushsim'])
