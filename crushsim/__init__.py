07bde80756246eb465ddf1b97d4ea9cc17bb7e57
Xavier Villaneau <xvillaneau@gmail.com>
2016-04-26 20:12:50 -0400

    Configuration management

--- crushsim/__init__.py
+++ crushsim/__init__.py
@@ -1,8 +1,9 @@
 
 from __future__ import print_function
+import utils
 
 
 class CRUSHsim():
 
-    def __init__():
-        pass
+    def __init__(self, cfg_file=None):
+        self.config = utils.load_config(cfg_file=None)
