aa729e15bf8ba4847d745ef05f11a0e676517cbb
Xavier Villaneau <xavier.villaneau@ccur.com>
2016-05-01 18:28:54 -0400

    More tests for Types

--- crushsim/testing/test_types.py
+++ crushsim/testing/test_types.py
@@ -25,12 +25,22 @@ class TestTypes(unittest.TestCase):
                              {'id': 0, 'name': 'osd'})
         self.assertIn({'id': 3, 'name': 'root'}, self.types.get())
 
+    def test_exists(self):
+        """Test success path for Types.exists()"""
+        self.types.add("root", 3)
+        self.assertTrue(self.types.exists(id=3))
+        self.assertTrue(self.types.exists(name='root'))
+        self.assertFalse(self.types.exists(id=0))
+        self.assertFalse(self.types.exists(name='osd'))
+
     def test_add_except(self):
         """Test exceptiosn returned by Types.add()"""
         with self.assertRaises(TypeError):
             self.types.add(71, 0)
         with self.assertRaises(TypeError):
             self.types.add("test", "string")
+        with self.assertRaises(ValueError):
+            self.types.add("", 0)
         self.types.add('osd', 0)
         with self.assertRaises(IndexError):
             self.types.add('test', 0)
@@ -39,8 +49,23 @@ class TestTypes(unittest.TestCase):
 
     def test_get_except(self):
         """Test exceptiosn returned by Types.get()"""
+        with self.assertRaises(TypeError):
+            self.types.get(name=71)
+        with self.assertRaises(TypeError):
+            self.types.get(id="string")
+        with self.assertRaises(ValueError):
+            self.types.get(name="")
         self.types.add('osd', 0)
         with self.assertRaises(ValueError):
             self.types.get(id=0, name='osd')
         with self.assertRaises(IndexError):
             self.types.get(id=1)
+
+    def test_exists_except(self):
+        """Test exceptiosn returned by Types.exists()"""
+        with self.assertRaises(TypeError):
+            self.types.exits(name=71)
+        with self.assertRaises(TypeError):
+            self.types.exists(id="string")
+        with self.assertRaises(ValueError):
+            self.types.exists(name="")
