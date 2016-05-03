a359383271e848be4c0a90d15fbc1d727de8716b
Xavier Villaneau <xavier.villaneau@ccur.com>
2016-05-03 11:06:56 -0400

    Basic tests for Buckets

--- crushsim/testing/test_buckets.py
+++ crushsim/testing/test_buckets.py
@@ -1,5 +1,7 @@
 
 import unittest
+from crushsim.map.devices import Devices
+from crushsim.map.Types import Types
 from crushsim.map.buckets import Buckets
 
 
@@ -8,5 +10,44 @@ class TestBuckets(unittest.TestCase):
     def setUp(self):
         self.buckets = Buckets()
 
+        self.devices = Devices()
+        self.devices.create_bunch(16)
+
+        self.types = Types()
+        self.types.create_set(['osd', 'host', 'psu', 'root'])
+
     def tearDown(self):
         self.buckets = None
+
+    def test_add(self):
+        b_dict_host0 = {
+            'name': 'host0',
+            'type': 'host',
+            'item': [
+                {'name': 'osd.0', 'weight': 1.0},
+                {'name': 'osd.1', 'weight': 1.0},
+                {'name': 'osd.2', 'weight': 1.0},
+                {'name': 'osd.3', 'weight': 1.0}
+            ]
+        }
+        b_dict_host1 = {
+            'name': 'host1',
+            'type': 'host',
+            'item': [
+                {'name': 'osd.4', 'weight': 1.0},
+                {'name': 'osd.5', 'weight': 1.0},
+                {'name': 'osd.6', 'weight': 1.0},
+                {'name': 'osd.7', 'weight': 1.0}
+            ]
+        }
+        b_dict_psu0 = {
+            'name': 'psu0',
+            'type': 'psu',
+            'item': [
+                {'name': 'host0'},
+                {'name': 'host1'}
+            ]
+        }
+        self.buckets.add(b_dict_host0)
+        self.buckets.add(b_dict_host1)
+        self.buckets.add(b_dict_psu0)
