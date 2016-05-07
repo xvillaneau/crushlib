f9e3cee3f2cca6a6c589377e7c7e77c9306f2420
Xavier Villaneau <xvillaneau@gmail.com>
2016-05-07 16:52:23 -0400

    More tests for the Buckets object

--- crushsim/testing/test_buckets.py
+++ crushsim/testing/test_buckets.py
@@ -2,7 +2,7 @@
 import unittest
 from crushsim.map.devices import Devices
 from crushsim.map.types import Types
-from crushsim.map.buckets import Buckets
+from crushsim.map.buckets import Buckets, Bucket
 
 
 class TestBuckets(unittest.TestCase):
@@ -19,8 +19,8 @@ class TestBuckets(unittest.TestCase):
     def tearDown(self):
         self.buckets = None
 
-    def test_add(self):
-        """Test success path for Buckets.add()"""
+    def test_addfromdict(self):
+        """Test success path for Buckets.add_from_dict()"""
         b_dict_host0 = {
             'name': 'host0',
             'type': 'host',
@@ -53,3 +53,31 @@ class TestBuckets(unittest.TestCase):
         self.buckets.add_from_dict(b_dict_host0)
         self.buckets.add_from_dict(b_dict_host1)
         self.buckets.add_from_dict(b_dict_psu0)
+
+    def test_get(self):
+        """Test for Buckets.get()"""
+        self.test_addfromdict()
+
+        b_host0 = self.buckets.get(name='host0')
+        self.assertIsInstance(b_host0, Bucket)
+        self.assertEqual(b_host0.name, 'host0')
+
+        b_host1 = self.buckets.get(id=-2)
+        self.assertIsInstance(b_host1, Bucket)
+        self.assertEqual(b_host1.name, 'host1')
+
+        with self.assertRaises(IndexError):
+            self.buckets.get(name='testABC')
+        with self.assertRaises(IndexError):
+            self.buckets.get(id=-71)
+
+    def test_nextid(self):
+        """Test for Buckets.next_id()"""
+        self.assertEqual(self.buckets.next_id(), -1)
+        self.test_addfromdict()
+        self.assertEqual(self.buckets.next_id(), -4)
+
+    def test_exists(self):
+        self.test_addfromdict()
+        self.assertTrue(self.buckets.exists('host0'))
+        self.assertFalse(self.buckets.exists('testABC'))
