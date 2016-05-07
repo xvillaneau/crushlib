02f445803098b6d971b0d523233741c1ae743913
Xavier Villaneau <xavier.villaneau@ccur.com>
2016-05-07 15:00:20 -0400

    Better tests for Devices

--- crushsim/testing/test_devices.py
+++ crushsim/testing/test_devices.py
@@ -11,25 +11,29 @@ class TestDevices(unittest.TestCase):
     def tearDown(self):
         self.dev = None
 
-    def test_add_nonum(self):
+    def test_add(self):
         """No argument given, then next available is created"""
         self.assertEqual(self.dev.add(), 0)
+        self.assertEqual(self.dev.add(2), 2)
         self.assertEqual(self.dev.add(), 1)
 
-    def test_add_specnum(self):
-        """Adding a device with specified number returns """
-        self.assertEqual(self.dev.add(2), 2)
-        self.assertEqual(self.dev.add(0), 0)
-
-        with self.assertRaises(IndexError):
-            self.dev.add(2)
-
-    def test_wrongarg(self):
+    def test_add_except(self):
         """Num should be a positive integer"""
         with self.assertRaises(ValueError):
             self.dev.add('string')
         with self.assertRaises(ValueError):
             self.dev.add(-1)
+        self.dev.add(2)
+        with self.assertRaises(IndexError):
+            self.dev.add(2)
+
+    def test_getnextnum(self):
+        self.assertEqual(self.dev.get_next_number(), 0)
+        self.dev.add()
+        self.dev.add(2)
+        self.assertEqual(self.dev.get_next_number(), 1)
+        self.dev.add()
+        self.assertEqual(self.dev.get_next_number(), 3)
 
     def test_create_bunch(self):
         """Testing behavior of Devices.create_bunch()"""
