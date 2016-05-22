4b707999736a899c2305e8eb21ef79abb16cc287
Xavier Villaneau <xvillaneau@gmail.com>
2016-05-22 12:46:41 -0400

    Tunables now have profiles and can be printed

--- crushsim/map/tunables.py
+++ crushsim/map/tunables.py
@@ -7,8 +7,58 @@ class Tunables():
 
     def __init__(self):
         self.settings = {}
-        self.preset = None
+        self.profile = None
+
+    def __str__(self):
+        print_order = [
+            'choose_local_tries',
+            'choose_local_fallback_tries',
+            'choose_total_tries',
+            'chooseleaf_descend_once',
+            'chooseleaf_vary_r',
+            'allowed_bucket_algs'
+        ]
+
+        out = ""
+        for k in print_order:
+            if self.settings.get(k) is not None:
+                out += 'tunable {} {}\n'.format(k, self.settings.get(k))
+        return out[:-1]
 
     def update_setting(self, name, value):
         self.settings.setdefault(name, None)
+        self.profile = None
         self.settings[name] = value
+
+    def set_profile(self, profile):
+
+        if profile in ('legacy', 'argonaut'):
+            self.settings = {}
+        if profile == 'bobtail':
+            self.settings = {
+                'choose_local_tries': 0,
+                'choose_local_fallback_tries': 0,
+                'choose_total_tries': 50,
+                'chooseleaf_descend_once': 1
+            }
+        if profile == 'firefly':
+            self.settings = {
+                'choose_local_tries': 0,
+                'choose_local_fallback_tries': 0,
+                'choose_total_tries': 50,
+                'chooseleaf_descend_once': 1,
+                'chooseleaf_vary_r': 1
+            }
+        if profile == 'hammer':
+            self.settings = {
+                'choose_local_tries': 0,
+                'choose_local_fallback_tries': 0,
+                'choose_total_tries': 50,
+                'chooseleaf_descend_once': 1,
+                'chooseleaf_vary_r': 1,
+                'allowed_bucket_algs': 54
+            }
+        else:
+            raise ValueError("Unknown profile {}".format(profile))
+
+        self.profile = profile
