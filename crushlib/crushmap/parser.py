1fd1a2ba60140c4e773256111729394dea50c20d
Xavier Villaneau <xvillaneau@gmail.com>
2016-05-23 22:04:03 -0400

    100% parser test coverage

--- crushlib/crushmap/parser.py
+++ crushlib/crushmap/parser.py
@@ -8,11 +8,11 @@ from crushlib.crushmap.buckets import Bucket
 
 def parse_raw(crushmap, map_obj):
     parsed = _raw_to_dict(crushmap)
-    parse_tunables(map_obj, parsed['tunable'])
-    parse_devices(map_obj, parsed['device'])
-    parse_types(map_obj, parsed['type'])
-    parse_buckets(map_obj, parsed['bucket'])
-    parse_rules(map_obj, parsed['rule'])
+    _parse_tunables(map_obj, parsed['tunable'])
+    _parse_devices(map_obj, parsed['device'])
+    _parse_types(map_obj, parsed['type'])
+    _parse_buckets(map_obj, parsed['bucket'])
+    _parse_rules(map_obj, parsed['rule'])
 
 
 def _raw_to_dict(raw_str):
@@ -41,38 +41,37 @@ def _raw_to_dict(raw_str):
                 raw_dict[head].append(l)
             else:
                 if not l.endswith('{'):
-                    raise ValueError("CRUSH Parsing error")
+                    raise ValueError("CRUSH Parsing error: Unopened block")
                 in_block = True
                 block = [l]
                 block_type = 'rule' if head == 'rule' else 'bucket'
                 raw_dict.setdefault(block_type, [])
+
+    if in_block:
+        raise ValueError("CRUSH Parsing error: Unclosed block")
+
     return raw_dict
 
 
-def parse_tunables(map_obj, tun_list):
+def _parse_tunables(map_obj, tun_list):
     for raw in tun_list:
         line = raw.split()
-        if line[0] != 'tunable':
-            raise ValueError("Tunable Parsing error: Line should begin "
-                             "with 'tunable'")
+
         try:
             name = line[1]
             value = int(line[2])
         except IndexError:
             raise ValueError("Tunable Parsing error: Tunable declaration "
-                             " is incomplete!")
+                             "is incomplete!")
         except ValueError:
             raise ValueError("Tunable Parsing error: Tunable value expected "
                              "to be an integer!")
         map_obj.tunables.update_setting(name, value)
 
 
-def parse_devices(map_obj, dev_list):
+def _parse_devices(map_obj, dev_list):
     for raw in dev_list:
         line = raw.split()
-        if line[0] != 'device':
-            raise ValueError("Device parsing error: Line should begin "
-                             "with 'device'")
 
         try:
             num = int(line[1])
@@ -86,16 +85,11 @@ def parse_devices(map_obj, dev_list):
 
         if name == ('osd.{}'.format(num)):
             map_obj.devices.add(num)
-        else:
-            continue
 
 
-def parse_types(map_obj, types_list):
+def _parse_types(map_obj, types_list):
     for string in types_list:
         line = string.split()
-        if line[0] != 'type':
-            raise ValueError(
-                "Types Parsing error: Line shoudl begin with 'type'")
 
         try:
             id = int(line[1])
@@ -110,7 +104,7 @@ def parse_types(map_obj, types_list):
         map_obj.types.add(name, id)
 
 
-def parse_buckets(crushmap, buckets_list):
+def _parse_buckets(crushmap, buckets_list):
 
     def parse_bucket(bucket_raw):
         items = []
@@ -146,7 +140,7 @@ def parse_buckets(crushmap, buckets_list):
         crushmap.buckets.add(parse_bucket(bucket_raw))
 
 
-def parse_rules(map_obj, rules_list):
+def _parse_rules(map_obj, rules_list):
     for rule_list in rules_list:
         steps = Steps()
 
---
+++ crushlib/testing/test_parser.py
@@ -0,0 +1,82 @@
+
+from __future__ import absolute_import, division, \
+                       print_function, unicode_literals
+import unittest
+
+from crushlib.crushmap import CRUSHmap
+from crushlib.crushmap.parser import _raw_to_dict, _parse_tunables, \
+                                     _parse_devices, _parse_types, \
+                                     _parse_buckets, _parse_rules
+
+
+class TestParser(unittest.TestCase):
+
+    def setUp(self):
+        self.map = CRUSHmap()
+
+    def tearDown(self):
+        self.map = None
+
+    def test_raw_parsing(self):
+        """Test parser._raw_to_dict()"""
+        with self.assertRaises(ValueError):
+            _raw_to_dict('host host0')
+        with self.assertRaises(ValueError):
+            _raw_to_dict('host host0 {')
+
+    def test_tunables_parsing(self):
+        """Test parser._parse_tunables()"""
+        with self.assertRaises(ValueError):
+            _parse_tunables(self.map, ['tunable test'])
+        with self.assertRaises(ValueError):
+            _parse_tunables(self.map, ['tunable test TEST'])
+
+    def test_devices_parsing(self):
+        """Test parser._parse_devices()"""
+        _parse_devices(self.map, ['device 0 osd.0'])
+        _parse_devices(self.map, ['device 1 device1'])
+        _parse_devices(self.map, ['device 2 osd.2'])
+        _parse_devices(self.map, ['device 3 osd.3'])
+        with self.assertRaises(ValueError):
+            _parse_devices(self.map, ['device 2'])
+        with self.assertRaises(ValueError):
+            _parse_devices(self.map, ['device test osd.2'])
+
+    def test_types_parsing(self):
+        """Test parser._parse_types()"""
+        _parse_types(self.map, ['type 0 osd'])
+        _parse_types(self.map, ['type 1 host'])
+        _parse_types(self.map, ['type 2 root'])
+        with self.assertRaises(ValueError):
+            _parse_types(self.map, ['type 3'])
+        with self.assertRaises(ValueError):
+            _parse_types(self.map, ['type test test'])
+
+    def test_buckets_parsing(self):
+        """Test parser._parse_buckets()"""
+        self.test_devices_parsing()
+        self.test_types_parsing()
+        _parse_buckets(self.map, [['host host0 {', 'id -1', 'alg straw',
+                                   'hash 0', 'item osd.0 weight 1.000']])
+        _parse_buckets(self.map, [['host host1 {', 'id -2', 'alg straw',
+                                   'hash 0', 'item osd.2 weight 1.000',
+                                   'item osd.3 weight 1.000']])
+        _parse_buckets(self.map, [['root root {', 'id -3', 'alg straw',
+                                   'hash 0', 'item host0 weight 1.000',
+                                   'item host1 weight 2.000']])
+        with self.assertRaises(ValueError):
+            _parse_buckets(self.map, [['hash 1']])
+        with self.assertRaises(ValueError):
+            _parse_buckets(self.map, [['test 71']])
+
+    def test_rules_parsing(self):
+        """Test parser._parse_rules()"""
+        self.test_buckets_parsing()
+        _parse_rules(self.map, [[
+            'rule replicated_ruleset {', 'ruleset 0', 'type replicated',
+            'min_size 1', 'max_size 10', 'step take root',
+            'step chooseleaf firstn 0 type host', 'step emit']])
+        with self.assertRaises(ValueError):
+            _parse_rules(self.map, [['step helloworld']])
+        with self.assertRaises(ValueError):
+            _parse_rules(self.map, [['test 71']])
