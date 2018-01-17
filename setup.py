574c8d5b3e2e3ac57a2c1a54e956100f5eccb0c4
Xavier Villaneau <xvillaneau@gmail.com>
2018-01-17 18:38:24 +0100

    Moved tests outside of library source

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 
 from setuptools import setup
 
-setup(name='crushlib', version='0.1', packages=['crushlib'],
-      test_suite='nose.collector', tests_require=['nose'],
-      )
+setup(
+    name='crushlib',
+    version='0.1.0',
+    packages=['crushlib'],
+)
