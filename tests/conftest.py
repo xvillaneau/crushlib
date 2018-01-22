5a1bcf1a10ad09ea0038321d2e84513cb0b16af7
Xavier Villaneau <xvillaneau@gmail.com>
2018-01-22 15:28:55 +0100

    Updated crushmap test to use pytest

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+
+import os
+import pytest
+
+from crushlib.crushmap import CrushMap
+
+FILES_DIR = os.path.join(os.path.dirname(__file__), 'files')
+
+
+@pytest.fixture
+def crushmap_empty():
+    c_map = CrushMap()
+    yield c_map
+
+
+@pytest.fixture
+def crushmap_missing_dev(crushmap_empty):
+    """:type crushmap_empty: CrushMap"""
+    crush_file = os.path.join(FILES_DIR, 'crushmap_missingdev.txt')
+    crushmap_empty.read_file(crush_file)
+    yield crushmap_empty
--- tests/test_crushmap.py
+++ tests/test_crushmap.py
@@ -2,59 +2,48 @@
 from __future__ import absolute_import, division, \
                        print_function
 import os
-import unittest
+import pytest
 
 from crushlib.crushmap import CrushMap, Type, Bucket
 
 FILES_DIR = os.path.join(os.path.dirname(__file__), 'files')
 
 
-class TestCRUSHmap(unittest.TestCase):
+class TestCRUSHmap(object):
 
-    def setUp(self):
-        self.crushmap = CrushMap()
-
-    def tearDown(self):
-        self.crushmap = None
-
-    def test_crusmap_import(self):
+    def test_crushmap_import(self, crushmap_empty):
         """Basic import success path for a simple map"""
-        crushfile = os.path.join(FILES_DIR, 'crushmap_complete.txt')
-        self.crushmap.read_file(crushfile)
+        crush_file = os.path.join(FILES_DIR, 'crushmap_complete.txt')
+        crushmap_empty.read_file(crush_file)
 
-        self.assertEqual(self.crushmap.devices.next_id(), 16)
+        assert 16 == crushmap_empty.devices.next_id()
+        assert isinstance(crushmap_empty.types.get_type(name='host'), Type)
 
-        self.assertIsInstance(self.crushmap.types.get_type(name='host'), Type)
+        host1 = crushmap_empty.buckets.get_bucket(name='host1')
+        assert isinstance(host1, Bucket)
+        assert -2 == host1.id
+        assert 4 == len(host1.items)
 
-        host1 = self.crushmap.buckets.get_bucket(name='host1')
-        self.assertIsInstance(host1, Bucket)
-        self.assertEqual(host1.id, -2)
-        self.assertEqual(len(host1.items), 4)
-
-    def test_import_missingdev(self):
+    def test_import_missingdev(self, crushmap_missing_dev):
         """Import of a map in which osd.7 is absent"""
-        crushfile = os.path.join(FILES_DIR, 'crushmap_missingdev.txt')
-        self.crushmap.read_file(crushfile)
-        self.assertEqual(self.crushmap.devices.next_id(), 7)
+        assert 7 == crushmap_missing_dev.devices.next_id()
 
-    def test_print_map(self):
+    def test_print_map(self, crushmap_missing_dev):
         """Test that maps are properly printed"""
-        crushfile = os.path.join(FILES_DIR, 'crushmap_missingdev.txt')
-        self.crushmap.read_file(crushfile)
-        self.assertEqual(self.crushmap.raw_map, str(self.crushmap))
+        assert crushmap_missing_dev.raw_map == str(crushmap_missing_dev)
 
     def test_get_item(self):
         """Test CRUSHmap.get_item()"""
         crushmap = CrushMap.create(4, [('host', 2)])
-        self.assertEqual(crushmap.get_item(name='osd.0').id, 0)
-        self.assertEqual(crushmap.get_item(item_id=-2).name, 'host1')
-        with self.assertRaises(IndexError):
+        assert 0 == crushmap.get_item(name='osd.0').id
+        assert 'host1' == crushmap.get_item(item_id=-2).name
+        with pytest.raises(IndexError):
             crushmap.get_item(item_id=-4)
 
     def test_crusmap_create(self):
         CrushMap.create(4, [('host', 2)])
         CrushMap.create(15, [('host', 4), ('psu', 3)])
         c = CrushMap.create(4, [('host', 2), ('myroot', 0)])
-        self.assertEqual(c.get_item(name='myroot').id, -3)
-        with self.assertRaises(ValueError):
+        assert -3 == c.get_item(name='myroot').id
+        with pytest.raises(ValueError):
             CrushMap.create(4, [('root', 2)])
