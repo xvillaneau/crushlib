f11ae70f4158931ac0ffda25fc270dec30d5a153
Xavier Villaneau <xvillaneau@gmail.com>
2018-01-23 20:21:36 +0100

    Converted bucket tests to py.test

--- tests/test_buckets.py
+++ tests/test_buckets.py
@@ -1,104 +1,96 @@
 
 from __future__ import absolute_import, division, \
                        print_function, unicode_literals
-import unittest
+import pytest
 from crushlib.crushmap import CrushMap, Bucket
 
 
-class TestBuckets(unittest.TestCase):
+class TestBuckets(object):
 
-    def setUp(self):
-        self.crushmap = CrushMap()
-        self.crushmap.devices.create_bunch(4)
-        self.crushmap.types.create_set(['osd', 'host', 'root'])
-
-    def tearDown(self):
-        self.crushmap = None
-
-    def test_buckets_add(self):
+    def test_buckets_add(self, crushmap):
         """Test Buckets.add_bucket()"""
 
-        host0 = Bucket('host0', self.crushmap.types.get_type('host'))
-        host0.add_item(self.crushmap.get_item('osd.0'))
-        host0.add_item(self.crushmap.get_item('osd.1'))
-        self.crushmap.buckets.add_bucket(host0)
+        host0a = Bucket('host0_a', crushmap.types.get_type('host'))
+        host0a.add_item(crushmap.get_item('osd.0'))
+        host0a.add_item(crushmap.get_item('osd.1'))
+        crushmap.buckets.add_bucket(host0a)
 
-        host1 = Bucket('host1', self.crushmap.types.get_type('host'))
-        host1.add_item(self.crushmap.get_item('osd.2'))
-        host1.add_item(self.crushmap.get_item('osd.3'))
-        self.crushmap.buckets.add_bucket(host1)
+        host1a = Bucket('host1_a', crushmap.types.get_type('host'))
+        host1a.add_item(crushmap.get_item('osd.2'))
+        host1a.add_item(crushmap.get_item('osd.3'))
+        crushmap.buckets.add_bucket(host1a)
 
-        root = Bucket('root', self.crushmap.types.get_type('root'))
-        root.add_item(host0)
-        root.add_item(host1)
-        self.crushmap.buckets.add_bucket(root)
+        root = Bucket('root_a', crushmap.types.get_type('root'))
+        root.add_item(host0a)
+        root.add_item(host1a)
+        crushmap.buckets.add_bucket(root)
 
-        with self.assertRaises(IndexError):
-            self.crushmap.buckets.add_bucket(
-                Bucket('root', self.crushmap.types.get_type('root')))
-        with self.assertRaises(IndexError):
-            self.crushmap.buckets.add_bucket(
-                Bucket('root2', self.crushmap.types.get_type('root'), bucket_id=-3))
+        with pytest.raises(IndexError):
+            crushmap.buckets.add_bucket(
+                Bucket('root', crushmap.types.get_type('root')))
+        with pytest.raises(IndexError):
+            crushmap.buckets.add_bucket(
+                Bucket('root2', crushmap.types.get_type('root'), bucket_id=-3))
 
-    def test_buckets_get(self):
-        """Test for Buckets.get_bucket()"""
-        self.test_buckets_add()
+    def test_buckets_get(self, crushmap):
+        """:type crushmap: CrushMap"""
 
-        host0 = self.crushmap.buckets.get_bucket(name='host0')
-        self.assertIsInstance(host0, Bucket)
-        self.assertEqual(host0.id, -1)
+        host0 = crushmap.buckets.get_bucket(name='host0')
+        assert isinstance(host0, Bucket)
+        assert -1 == host0.id
 
-        host1 = self.crushmap.buckets.get_bucket(bucket_id=-2)
-        self.assertIsInstance(host1, Bucket)
-        self.assertEqual(host1.name, 'host1')
+        host1 = crushmap.buckets.get_bucket(bucket_id=-2)
+        assert isinstance(host1, Bucket)
+        assert 'host1' == host1.name
 
-        l = self.crushmap.buckets.get_bucket()
-        self.assertIsInstance(l, list)
-        self.assertIsInstance(l[0], Bucket)
+        l = crushmap.buckets.get_bucket()
+        assert isinstance(l, list)
+        assert isinstance(l[0], Bucket)
 
-        with self.assertRaises(ValueError):
-            self.crushmap.buckets.get_bucket(bucket_id=-1, name='host0')
-        with self.assertRaises(IndexError):
-            self.crushmap.buckets.get_bucket(name='testABC')
-        with self.assertRaises(IndexError):
-            self.crushmap.buckets.get_bucket(bucket_id=-71)
+        with pytest.raises(ValueError):
+            crushmap.buckets.get_bucket(bucket_id=-1, name='host0')
+        with pytest.raises(IndexError):
+            crushmap.buckets.get_bucket(name='testABC')
+        with pytest.raises(IndexError):
+            crushmap.buckets.get_bucket(bucket_id=-71)
 
-    def test_buckets_nextid(self):
+    def test_buckets_nextid_empty(self, crushmap_empty):
         """Test for Buckets.next_id()"""
-        self.assertEqual(self.crushmap.buckets.next_id(), -1)
-        self.test_buckets_add()
-        self.assertEqual(self.crushmap.buckets.next_id(), -4)
+        assert -1 == crushmap_empty.buckets.next_id()
 
-    def test_buckets_exists(self):
+    def test_buckets_nextid(self, crushmap):
+        """Test for Buckets.next_id()"""
+        assert -8 == crushmap.buckets.next_id()
+
+    def test_buckets_exists(self, crushmap):
         """Test for Buckets.bucket_exists()"""
-        self.test_buckets_add()
-        self.assertTrue(self.crushmap.buckets.bucket_exists('host0'))
-        self.assertFalse(self.crushmap.buckets.bucket_exists('testABC'))
+        assert crushmap.buckets.bucket_exists('host0') is True
+        assert crushmap.buckets.bucket_exists('testABC') is False
 
-    def test_bucket_init(self):
-        t = self.crushmap.types.get_type('host')
+    def test_bucket_init(self, crushmap):
+        t = crushmap.types.get_type('host')
 
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             Bucket('test', t, bucket_id=0)
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             Bucket('test', t, alg='test')
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             Bucket('test', t, crush_hash='test')
 
-    def test_bucket_additem(self):
-        t = self.crushmap.types.get_type('host')
+    def test_bucket_additem(self, crushmap):
+        t = crushmap.types.get_type('host')
         b = Bucket('test', t)
 
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             b.add_item('test')
 
-    def test_bucket_weight(self):
+    def test_bucket_weight(self, crushmap):
 
-        t = self.crushmap.types.get_type('host')
+        t = crushmap.types.get_type('host')
         b1 = Bucket('test1', t)
         b2 = Bucket('test2', t)
         b1.add_item(b2)
         b2.add_item(b1)
 
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             b1.weight()
