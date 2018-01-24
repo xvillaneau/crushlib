862b94bbc9414282addd3c385de3ab1894d43dca
Xavier Villaneau <xvillaneau@gmail.com>
2018-01-24 17:47:27 +0100

    Added license and more package metadata

--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@ setup(
     name='crushlib',
     version='0.1.0',
     packages=['crushlib'],
+    license='GNU LGPLv3 ',
+    author='Xavier Villaneau',
+    author_email='xvillaneau@gmail.com',
+    description="Various tools for manipulating CRUSH maps in Ceph",
+    keywords=['ceph', 'crush']
 )
